8eadee408fd6541e824fb5c7a3602166a4b6b7e0
Sumit Panday <sumitpanday11052004@gmail.com>
2026-08-18 08:32:49 +0530

    security: prevent response caching

--- app/main.py
+++ app/main.py
@@ -72,7 +72,8 @@ async def add_security_headers(request: Request, call_next):
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["X-Frame-Options"] = "DENY"
     response.headers["Referrer-Policy"] = "no-referrer"
-
+    response.headers["Cache-Control"] = "no-store"
+    
     return response
 
 
